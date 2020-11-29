65a85320f715cec6ede2180f693663b678756da5
Anton Suprunov <suprunov.anton@gmail.com>
2020-11-30 04:05:33 +0500

    preloading

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import useAlbum from "../../queries/useAlbum";
+
+const preloadImages = async (arr) => {
+  const queue = arr.map(src => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    })
+  });
+
+  return await Promise.all(queue);
+}
 
 const Album = ({ album }) => {
   console.log(album);
   const photos = useAlbum(album.key);
-  console.log(photos)
+  useEffect(() => {
+    const init = async () => {
+      const preloadArray = photos.map(slide => {
+        return [...slide['_0'], ...slide['_1']].map(i => `/photos/${album.key}/${i}.jpg`);
+      }).flat();
+      console.log(preloadArray)
+      await preloadImages(preloadArray);
+      console.log('--IMAGES LOADED--')
+    }
+    init();
+  }, []);
+  
+
+
+  //console.log(photos)
   return <div>
-    {photos.map(slide => <div>
-      {/*<a href={`/static/${p}`}></a>*/}
-      {slide['_0'].map(i => <img src={`/photos/${album.key}/${i}.jpg`} style={{maxWidth: '100px'}} />)}
-      <br /><br />
-      {slide['_1'].map(i => <img src={`/photos/${album.key}/${i}.jpg`} style={{maxWidth: '100px'}} />)}
-    </div>)}
+    {
+      photos.map((slide, i) => <div key={`slide-${i}`}>
+        {slide['_0'].map(j => <img key={`slide-1-${j}`} src={`/photos/${album.key}/${j}.jpg`} style={{maxWidth: '100px'}} />)}
+        <br /><br />
+        {slide['_1'].map(j => <img key={`slide-2-${j}`} src={`/photos/${album.key}/${j}.jpg`} style={{maxWidth: '100px'}} />)}
+      </div>)
+    }
   </div>;
 }
 
